90ebecfc6b2338270302b355086b06b532a91c35
VuzX <phamnguyenvu110905@gmail.com>
2026-03-26 13:14:48 +0700

    feat: update database connection configuration to use SSL and remove IPv4 setting

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,9 @@ require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 const pool = process.env.DATABASE_URL
     ? new Pool({
         connectionString: process.env.DATABASE_URL,
-        family: 4, // Use IPv4
+        ssl: {
+            rejectUnauthorized: false,
+        },
     })
     : new Pool({
         user: process.env.DB_USER || "postgres",
@@ -14,7 +16,6 @@ const pool = process.env.DATABASE_URL
         database: process.env.DB_NAME || "aitalab",
         password: process.env.DB_PASSWORD || "",
         port: Number(process.env.DB_PORT || 5432),
-        family: 4, // Use IPv4
     });
 
 module.exports = pool;
